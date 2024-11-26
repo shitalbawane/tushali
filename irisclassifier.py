bbe96eaf7e41d26e2ac306028ab10ffd72f74718
shital bawane <shitalbawane97@gmail.com>
2024-11-26 12:05:54 +0000

    change test size to 0.1

--- irisclassifier.py
+++ irisclassifier.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 df = pd.read_csv("/content/tushali/Data/Iris.csv")
 features =['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']
 target = ['Species']
-X_train,X_test,y_train,y_test = train_test_split(df[features],df[target], test_size=0.2,shuffle =True)
+X_train,X_test,y_train,y_test = train_test_split(df[features],df[target], test_size=0.1,shuffle =True)
 
 clf=DecisionTreeClassifier(criterion='entropy')
 clf.fit(X_train,y_train)
